c03fbbb927c7c601ed31a5a98d899d35127f3598
ygilm <ygilm@bk.ru>
2023-08-17 05:57:19 +0400

    fixed method contacts()

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,9 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'У вас новый запрос на обратную связь: {name} {phone}: {message}')
     return render(request, 'catalog/contacts.html')
