14dbe9542eeea769c95f33c2e19f9c2ac15ef720
ygilm <ygilm@bk.ru>
2023-08-20 23:04:59 +0400

    Changed

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,11 +4,11 @@ from catalog.models import Category, Product
 
 @admin.register(Category)
 class Category(admin.ModelAdmin):
-    list_display = ('id', 'category_name')
+    list_display = ('id', 'category_name', 'description',)
 
 
 @admin.register(Product)
 class Product(admin.ModelAdmin):
-    list_display = ('id', 'category')
-    list_filter = ('category',)
+    list_display = ('id', 'product_name', 'description', 'category', 'purchase_price',)
+    list_filter = ('category', 'purchase_price',)
     search_fields = ('product_name', 'description',)
