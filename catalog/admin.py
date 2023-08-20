1cc39bcc7b0bf1e3cd6ed704c1ee3adae77ee6ee
ygilm <ygilm@bk.ru>
2023-08-20 17:12:03 +0400

    Added initial migration for category and product models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from catalog.models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    list_display = ('id', 'category_name')
+
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    list_display = ('id', 'category')
+    list_filter = ('category',)
+    search_fields = ('product_name', 'description',)
