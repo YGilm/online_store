b3403e30f5d47acb5176099e882f2279bff2ca11
ygilm <ygilm@bk.ru>
2023-09-06 01:06:36 +0400

    Added new urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,17 +2,21 @@ from django.urls import path
 from django.conf import settings
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product
+from catalog.views import *
 from django.conf.urls.static import static
-from catalog import views
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', HomeListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('product/', product, name='product'),
-    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('product/', ProductListView.as_view(), name='product_list'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('blogs/', BlogPostListView.as_view(), name='blogs'),
+    path('blogs/<int:pk>/', BlogPostDetailView.as_view(), name='blogpost_detail'),
+    path('create/', BlogPostCreateView.as_view(), name='blogpost_form'),
+    path('blogs/edit/<int:pk>/', BlogPostUpdateView.as_view(), name='blogpost_update'),
+    path('blogs/delete/<int:pk>/', BlogPostDeleteView.as_view(), name='blogpost_delete')
 ]
 
 if settings.DEBUG:
