eacd7c1a3ee2b3f137c4032a864efd2261d8cf57
ygilm <ygilm@bk.ru>
2023-09-27 01:38:02 +0400

    Changed

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from autoslug import AutoSlugField
 
+from users.models import User
+
 NULLABLE = {'null': True, 'blank': True}
 
 
@@ -32,6 +34,8 @@ class Product(models.Model):
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена за покупку')
     creation_date = models.DateField(auto_now_add=True, verbose_name='дата создания')
     last_modified_date = models.DateField(auto_now=True, verbose_name='дата последнего изменения')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь', **NULLABLE)
 
     def __str__(self):
         return f'{self.product_name}, {self.description} {self.purchase_price}'
