42eec6a3bb5ffaaf394f4c0f43dc71fed3204ea5
ygilm <ygilm@bk.ru>
2023-10-02 18:06:48 +0400

    Added new settings of the cache

--- config/settings.py
+++ config/settings.py
@@ -11,28 +11,29 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-
 import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Загрузка переменных окружения из файла .env
+# Load environment variables from the .env file
 dotenv.read_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# ==============================
+# SECURITY CONFIGURATIONS
+# ==============================
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
+# ==============================
+# APPLICATION DEFINITION
+# ==============================
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -46,6 +47,10 @@ INSTALLED_APPS = [
     'users',
 ]
 
+# ==============================
+# MIDDLEWARE CONFIGURATIONS
+# ==============================
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -55,6 +60,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# ==============================
+# URL AND TEMPLATE CONFIGURATIONS
+# ==============================
 
 ROOT_URLCONF = 'config.urls'
 
@@ -76,8 +85,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
+# ==============================
+# DATABASE CONFIGURATIONS
+# ==============================
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
@@ -91,8 +101,9 @@ DATABASES = {
     }
 }
 
-
-# Password validation
+# ==============================
+# PASSWORD VALIDATION
+# ==============================
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -110,8 +121,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
+# ==============================
+# INTERNATIONALIZATION
+# ==============================
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'ru-ru'
@@ -122,8 +134,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
+# ==============================
+# STATIC AND MEDIA FILES
+# ==============================
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
@@ -132,20 +145,29 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
-# Default primary key field type
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# ==============================
+# DEFAULT MODEL FIELD CONFIGURATIONS
+# ==============================
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = BASE_DIR / 'media'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# ==============================
+# AUTHENTICATION AND USER MODEL
+# ==============================
 
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/users/'
+
+# ==============================
+# EMAIL CONFIGURATIONS
+# ==============================
 
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
@@ -155,3 +177,17 @@ EMAIL_USE_SSL = False  # Использование SSL
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')  # Адрес электронной почты, который будет виден в поле "От"
+
+# ==============================
+# CACHE CONFIGURATIONS
+# ==============================
+
+CACHE_ENABLED = os.getenv('CACHE_ENABLED')
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": os.getenv("CACHES_LOCATION"),
+            # "TIMEOUT": 300  # Ручная регулировка времени жизни кеша в секундах
+        }
+    }
