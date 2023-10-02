ceee4318c98f824f86afc8fed3f69e788241cdff
ygilm <ygilm@bk.ru>
2023-10-02 18:21:36 +0400

    Added site caching settings

--- config/settings.py
+++ config/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
 # ==============================
 
 MIDDLEWARE = [
+    'django.middleware.cache.UpdateCacheMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 # ==============================
